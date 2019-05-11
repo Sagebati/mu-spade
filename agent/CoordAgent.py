8a671333c85c000ed51a591d780c756b75a04110
Samuel Batissou <samuelbatissou@gmail.com>
2019-05-11 15:14:00 +0200

    Contacts is now optional in coordagent

--- agent/CoordAgent.py
+++ agent/CoordAgent.py
@@ -2,7 +2,7 @@ from spade.agent import Agent
 
 
 class CoordAgent(Agent):
-    def __init__(self, jid, password, actions: [], contacts: [Agent], verify_security=False):
+    def __init__(self, jid, password, actions: [], contacts: [Agent] = [], verify_security=False):
         super().__init__(jid, password, verify_security)
         self.actions = actions
         self.contacts = contacts
